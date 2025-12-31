2e2d5bb52d26f2a21389ca9acaab8ae5ac6ae2bc
Crystal Spider <riccardo.chimisso@gmail.com>
2025-12-31 12:43:14 +0100

    Port to 1.21.11.

--- common/src/main/java/it/crystalnest/torch_hit/handler/AttackHandler.java
+++ common/src/main/java/it/crystalnest/torch_hit/handler/AttackHandler.java
@@ -13,7 +13,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.coppergolem.CopperGolem;
+import net.minecraft.world.entity.animal.golem.CopperGolem;
 import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
